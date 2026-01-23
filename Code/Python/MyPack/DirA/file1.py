b0bfe1a69feaf012922ed27616f0cbb0662eb993
Dora Di <tdi@cphbusiness.dk>
2026-01-23 17:23:05 +0100

    init 2026

--- Code/Python/MyPack/DirA/file1.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-# enable appending the path of the parent directory
-sys.path.append("..")
-
-
-# now you can address objects from siblings'directory (DirB)
--- Code/Readers/readweb/api.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def readAPI(url, params, headers):  
-    response = requests.get(url, params=params, headers=headers).json()
-    return response
