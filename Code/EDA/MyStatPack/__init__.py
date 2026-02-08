d485838f570893391d75e2f45fb076afdcbc13a7
Dora Di <tdi@cphbusiness.dk>
2026-02-08 13:39:46 +0100

    stat-code

---
+++ Code/EDA/MyStatPack/__init__.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+__all__ = ['module1', 'module2', 'module3', 'module4']
+
+from . import module1, module2, module3, module4
+
+
+def data(start, stop, rows, cols):
+    # np.random.uniform(start,stop,(rows,columns)
+    df = pd.DataFrame(np.random.randint(start,stop,(rows,cols)))
+    df = df.add_prefix('col_')
+    return df
