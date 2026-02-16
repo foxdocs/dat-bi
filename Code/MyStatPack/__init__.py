70f27b6f3d3f03e180ca9d708fc878c0e633b3ee
Dora Di <tdi@cphbusiness.dk>
2026-02-16 02:53:07 +0100

    stat

--- Code/MyStatPack/__init__.py
+++ Code/MyStatPack/__init__.py
@@ -8,6 +8,11 @@ from . import module1, module2, module3, module4
 
 def data(start, stop, rows, cols):
     # np.random.uniform(start,stop,(rows,columns)
-    df = pd.DataFrame(np.random.randint(start,stop,(rows,cols)))
-    df = df.add_prefix('col_')
+    df = pd.DataFrame(np.random.randint(start,stop,(rows,cols)))    
+    df = df.add_prefix('col')
+    return df
+
+def bell(median, stdiv, rows, cols):
+    df = pd.DataFrame(np.random.normal(median, stdiv, (rows,cols)))
+    df = df.add_prefix('col')
     return df
